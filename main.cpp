6b8953a32e51f9b2d2180a70922cd4de8db72924
Cary Phillips <cary@ilm.com>
2024-01-25 15:30:06 -0800

    test1
    Signed-off-by: Cary Phillips <cary@ilm.com>

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <Python.h>
+#include <iostream>
+
+static PyObject *hello(PyObject *self, PyObject *args)
+{
+    std::cout << "hello, world." << std::endl;
+    
+    Py_RETURN_NONE;
+}
+
+
+static PyMethodDef methods[] = {
+  {"hello", hello, METH_VARARGS},
+  {NULL, NULL},
+};
+
+PyMODINIT_FUNC PyInit_cmake_example(void)
+{
+    static struct PyModuleDef moduledef = {                   
+        PyModuleDef_HEAD_INIT, "cmake_example", "", -1, methods,
+    }; 
+
+    PyObject *m = PyModule_Create(&moduledef);
+
+    return m;
+}
+
