af0733385891b12f69a408c1f790e913750e774c
Cary Phillips <cary@ilm.com>
2024-01-25 17:13:40 -0800

    test7
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 import scikit_build_example as m
 
-def test_version():
-    assert m.__version__ == "0.1.3"
-
-
 def test_hello():
     m.hello()
 
+test_hello()
