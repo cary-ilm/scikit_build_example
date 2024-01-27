ba6b98c5e44178dcf8191d660b2a995c0edaf9c2
Cary Phillips <cary@ilm.com>
2024-01-26 18:40:35 -0800

    test14
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/cary_build_example/__init__.py
+++ src/cary_build_example/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from .XXX import __doc__, __version__, hello
+from ._XXX import __doc__, __version__, hello
 
 __all__ = ["__doc__", "__version__", "hello"]
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Python.h>
 #include <iostream>
 
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
 static PyObject *hello(PyObject *self, PyObject *args)
 {
     std::cout << "hello, world." << std::endl;
@@ -8,19 +11,20 @@ static PyObject *hello(PyObject *self, PyObject *args)
     Py_RETURN_NONE;
 }
 
-
 static PyMethodDef methods[] = {
   {"hello", hello, METH_VARARGS},
   {NULL, NULL},
 };
 
-PyMODINIT_FUNC PyInit_XXX(void)
+PyMODINIT_FUNC PyInit__XXX(void)
 {
     static struct PyModuleDef moduledef = {                   
         PyModuleDef_HEAD_INIT, "cary_build_example", "", -1, methods,
     }; 
 
     PyObject *m = PyModule_Create(&moduledef);
+
+    PyModule_AddStringConstant(m, "__version__", MACRO_STRINGIFY(VERSION_INFO));
 
     return m;
 }
